e0c8cd3953ae60ff25b82c617a4c929a222e70c8
pandeyankit11 <74775903+pandeyankit11@users.noreply.github.com>
2026-04-05 14:44:54 +0530

    Refactor database connection configuration

--- db.js
+++ db.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const db = new Pool({
-  host: process.env.DB_HOST || '10.30.67.122',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'expense_splitter_db',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'Ankitdatabase@1978',
+  host: process.env.DB_HOST ,
+  port: process.env.DB_PORT ,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
